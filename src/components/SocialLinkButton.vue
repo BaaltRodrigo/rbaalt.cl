8abbc9fe44a3878690ea49ff678a94d8eee91399
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-24 17:08:27 -0300

    Button link to redirect to socials

---
+++ src/components/SocialLinkButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn
+    :href="link"
+    variant="tonal"
+    target="_blank"
+    class="rounded-xl"
+    :color="color"
+    block
+    height="100%"
+    min-height="150"
+  >
+    <v-icon size="84">{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script setup>
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: "white",
+  },
+});
+</script>
