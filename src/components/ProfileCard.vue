110644914ebac70e3759983d4116542ccfa55572
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-24 17:08:13 -0300

    Basic profile info card

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card
+    variant="tonal"
+    class="rounded-xl pt-6 px-4"
+    color="purple"
+    height="100%"
+  >
+    <v-card-title class="text-h4">Hi! I'm Rodrigo </v-card-title>
+    <v-card-text>
+      <p class="my-1 text-h6">
+        +5 years as a full stack developer using Laravel + Vue.js to deliver
+        user friendly solutions.
+      </p>
+      <p class="my-1 text-h6">Currently working @Facilita</p>
+    </v-card-text>
+  </v-card>
+</template>
