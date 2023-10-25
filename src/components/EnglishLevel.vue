ffda6e01c952fc360bdd5cfabfd3f77396cce2af
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-25 13:03:19 -0300

    English level

---
+++ src/components/EnglishLevel.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card
+    class="d-flex flex-column justify-center align-center rounded-xl"
+    variant="tonal"
+    :height="height"
+    color="deep-purple-accent-4"
+  >
+    <!-- <div class="text-rotated text-center"> -->
+    <p class="text-subtitle-1">English level</p>
+    <v-card-title class="text-h4">B2</v-card-title>
+    <!-- </div> -->
+  </v-card>
+</template>
+
+<script setup>
+const props = defineProps({
+  height: {
+    type: String,
+    default: "100%",
+  },
+});
+</script>
