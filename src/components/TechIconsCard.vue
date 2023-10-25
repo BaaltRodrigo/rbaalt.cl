89a314a26db4e33e3e660919a5de043206ecc5e2
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-25 01:34:34 -0300

    Tech icons has a judo one

---
+++ src/components/TechIconsCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card variant="tonal" class="rounded-xl" height="100%">
+    <v-card-item>
+      <v-row align="center">
+        <v-col cols="3" v-for="icon in icons" :key="'tech-tool-' + icon">
+          <v-icon size="28">{{ icon }}</v-icon>
+        </v-col>
+        <v-col cols="3">
+          <p class="text-h6">🥋</p>
+        </v-col>
+      </v-row>
+    </v-card-item>
+  </v-card>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const icons = ref([
+  "mdi-github",
+  "mdi-git",
+  "mdi-language-php",
+  "mdi-laravel",
+  "mdi-vuejs",
+  "mdi-vuetify",
+  "mdi-language-javascript",
+  "mdi-angular",
+  "mdi-ubuntu",
+  "mdi-firebase",
+  "mdi-language-python",
+  "mdi-nodejs",
+  "mdi-microsoft-excel",
+]);
+</script>
