e712de5b47aee0ceb82ed59cd0d4201ac0de6c62
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-25 01:27:05 -0300

    Card to display my age

---
+++ src/components/YourAge.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    height="100%"
+    class="rounded-xl text-center pa-8"
+    variant="tonal"
+    color="green"
+  >
+    <p class="text-subtitle-1">My Age</p>
+    <v-card-title class="text-h4">{{ currentAge }}</v-card-title>
+  </v-card>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const birthDate = ref(new Date("1995-11-07"));
+
+const currentAge = computed(() => {
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.value.getFullYear();
+  const month = today.getMonth() - birthDate.value.getMonth();
+  // current day correction
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+});
+</script>
