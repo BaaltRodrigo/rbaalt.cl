109a26372c6f5ee9979cc9b22f768d7f35d50c3a
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-25 13:10:59 -0300

    Remove commented tags

--- src/components/EnglishLevel.vue
+++ src/components/EnglishLevel.vue
@@ -5,10 +5,8 @@
     :height="height"
     color="deep-purple-accent-4"
   >
-    <!-- <div class="text-rotated text-center"> -->
     <p class="text-subtitle-1">English level</p>
     <v-card-title class="text-h4">B2</v-card-title>
-    <!-- </div> -->
   </v-card>
 </template>
 
