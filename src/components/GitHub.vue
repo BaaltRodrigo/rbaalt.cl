15def6d9d9973a35c68a2b550b2b9993dc10428b
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-25 02:19:16 -0300

    Box to github

---
+++ src/components/GitHub.vue
@@ -0,0 +1,33 @@
+<template>
+  <a
+    href="https://github.com/BaaltRodrigo"
+    target="_blank"
+    class="rounded-xl text-decoration-none"
+  >
+    <v-card
+      @click=""
+      :height="height"
+      variant="tonal"
+      color="red"
+      class="rounded-xl"
+    >
+      <v-img cover width="100%" src="coding-guy.gif" class="text-black">
+        <v-toolbar color="transparent" class="px-2">
+          <template #prepend>
+            <v-icon size="28">mdi-github</v-icon>
+          </template>
+          <v-toolbar-title>Check on GitHub!</v-toolbar-title>
+        </v-toolbar>
+      </v-img>
+    </v-card>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  height: {
+    type: String,
+    default: "100%",
+  },
+});
+</script>
