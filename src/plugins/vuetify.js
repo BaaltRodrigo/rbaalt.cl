5ced766245ab72427bceb8659fbc735410bc4298
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-28 02:13:11 -0300

    Custom defaults for cards and buttons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,8 +7,11 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import { customDefaults } from "./vuetify/defaults.js";
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   components,
   directives,
+  defaults: customDefaults,
 });
