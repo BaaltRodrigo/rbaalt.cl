9cd84eaa8794b6e563e3b9de0b767e9885d35dca
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-01-22 19:29:35 -0300

    Add working experience and some color

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useDisplay } from "vuetify/lib/framework.mjs";
 import { computed } from "vue";
+import WorkingWith from "./components/WorkingWith.vue";
 
 const { md, lg, xl, xs } = useDisplay();
 
@@ -15,20 +16,26 @@ const isXSmallDisplay = computed(() => {
 
 <template>
   <v-app>
-    <v-main class="bg-grey-darken-3">
+    <v-main class="bg-grey-darken-4">
       <v-container class="pt-8" style="max-width: 75rem">
         <!--Top Section with big box and 2 on the next column-->
         <section class="py-12">
           <v-row :dense="isXSmallDisplay">
             <v-col cols="12" md="8">
-              <v-card class="pa-4 d-flex align-end" height="40vh">
+              <v-card
+                class="pa-4 d-flex align-end"
+                height="40vh"
+                color="grey-darken-3 text-grey-lighten-2"
+              >
                 <v-card-item>
                   <v-card-title>
                     <h1 class="text-h5">Rodrigo Pizarro Regueiro</h1>
                   </v-card-title>
-                  <v-card-subtitle class="text-body-1 font-weight-bold">
+                  <span
+                    class="text-body-1 text-amber-lighten-3 font-weight-bold"
+                  >
                     Software Engineer
-                  </v-card-subtitle>
+                  </span>
                   <div class="mt-2">
                     <v-btn
                       variant="flat"
@@ -55,7 +62,11 @@ const isXSmallDisplay = computed(() => {
             <v-col cols="12" md="4" class="d-flex">
               <v-row :dense="isXSmallDisplay">
                 <v-col cols="12">
-                  <v-card class="pt-4 px-4 pb-md-0 pb-4" height="100%">
+                  <v-card
+                    class="pt-4 px-4 pb-md-0 pb-4"
+                    height="100%"
+                    color="grey-darken-3 text-grey-lighten-1"
+                  >
                     <v-card-item>
                       <v-card-title>
                         <h2 class="text-h5">About Me</h2>
@@ -63,14 +74,14 @@ const isXSmallDisplay = computed(() => {
                     </v-card-item>
                     <v-card-text class="text-pretty">
                       Judoka and Software Engineer focused on deliver user
-                      friendly solutions while maintaining a high quality
-                      standard.
+                      friendly solutions
                     </v-card-text>
                   </v-card>
                 </v-col>
                 <v-col cols="6">
                   <v-sheet
                     height="100%"
+                    color="grey-darken-3 text-grey-lighten-1"
                     class="pa-3 rounded-xl d-flex flex-column justify-center align-center"
                   >
                     <p class="text-h2 text-center">+5</p>
@@ -82,8 +93,26 @@ const isXSmallDisplay = computed(() => {
                   </v-sheet>
                 </v-col>
                 <v-col cols="6">
-                  <v-card height="100%">
-                    <v-card-text class="text-center">No idea</v-card-text>
+                  <v-card
+                    height="100%"
+                    class="obscure-to-bottom"
+                    color="grey-darken-3 text-grey-lighten-4"
+                  >
+                    <!-- div to add opacity to the end of the component with a gradient -->
+                    <v-img
+                      src="/santiago-dark.png"
+                      cover
+                      width="100%"
+                      style="position: absolute"
+                    ></v-img>
+                    <div class="obscure-to-bottom"></div>
+                    <v-card-item class="align-self-end">
+                      <v-card-subtitle>
+                        <h3 class="text-center text-body-2">
+                          From Santiago, CL
+                        </h3>
+                      </v-card-subtitle>
+                    </v-card-item>
                   </v-card>
                 </v-col>
               </v-row>
@@ -93,49 +122,36 @@ const isXSmallDisplay = computed(() => {
         <!-- Last Working experiences -->
         <section class="py-4">
           <div class="text-center">
-            <h2 class="text-h4">Working experiences</h2>
-            <p class="text-body-1 mt-1 mb-5">Companies I have worked with</p>
+            <h2 class="text-h4 text-grey-lighten-2">Working experiences</h2>
+            <p class="text-body-1 mt-1 mb-5 text-grey-lighten-2">
+              Companies I have worked with
+            </p>
           </div>
           <!-- Cards of experiences -->
           <v-row justify="center">
             <v-col cols="12" sm="6" md="4">
-              <v-sheet
-                class="rounded-xl py-md-4 py-8 px-md-0 px-6 text-md-center"
-                color="white"
-                :height="isLargeDisplay ? '50vh' : ''"
-              >
-                <h3 class="text-h5">Facilita SPA</h3>
-                <h4 class="text-subtitle-2 font-weight-bold">
-                  Software Engineer
-                </h4>
-                <span class="text-body-2">Mar. 2019 - Ene. 2024</span>
-              </v-sheet>
+              <WorkingWith
+                company="Facilita SPA"
+                position="Software Engineer"
+                dates="Mar. 2019 - Ene. 2024"
+                image="/facilita-logo-dark.png"
+              />
             </v-col>
             <v-col cols="12" sm="6" md="4">
-              <v-sheet
-                class="rounded-xl py-md-4 py-8 px-md-0 px-6 text-md-center"
-                color="white"
-                :height="isLargeDisplay ? '50vh' : ''"
-              >
-                <h3 class="text-h5">CronApp</h3>
-                <h4 class="text-subtitle-2 font-weight-bold">
-                  Tech Lead / Full Stack Developer
-                </h4>
-                <span class="text-body-2">May. 2022 - Ago. 2023</span>
-              </v-sheet>
+              <WorkingWith
+                company="CronApp"
+                position="Tech Lead / Full Stack Developer"
+                dates="May. 2022 - Ago. 2023"
+                image="/cronapp-logo.png"
+              />
             </v-col>
             <v-col cols="12" sm="6" md="4">
-              <v-sheet
-                class="rounded-xl py-md-4 py-8 px-md-0 px-6 text-md-center"
-                color="white"
-                :height="isLargeDisplay ? '50vh' : ''"
-              >
-                <h3 class="text-h5">Ejercito de Chile</h3>
-                <h4 class="text-subtitle-2 font-weight-bold">
-                  Full Stack Developer
-                </h4>
-                <span class="text-body-2">Sept 2019 - Mar. 2020</span>
-              </v-sheet>
+              <WorkingWith
+                company="Ejercito de Chile"
+                position="Full Stack Developer"
+                dates="Sept 2019 - Mar. 2020"
+                image="/chilean-army-logo.png"
+              />
             </v-col>
           </v-row>
         </section>
@@ -160,6 +176,19 @@ const isXSmallDisplay = computed(() => {
 </template>
 
 <style lang="css">
+.obscure-to-bottom {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50%;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.5) 100%
+  );
+}
+
 .text-pretty {
   text-wrap: pretty;
 }
