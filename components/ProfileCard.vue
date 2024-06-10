4fb0dfc511d43f1862274673ba0e42eed45c1183
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-06-09 23:56:01 -0400

    🐛Prevent hydration miss match

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -5,47 +5,49 @@
     color="purple-darken-3 text-white"
     variant="outlined"
   >
-    <v-overlay
-      :model-value="true"
-      contained
-      no-click-animation
-      persistent
-      class="align-end"
-      :scrim="false"
-    >
-      <v-card-item class="ml-4 mb-4">
-        <v-card-title>
-          <h1 class="text-h4 text-white font-weight-bold">
-            Rodrigo Pizarro Regueiro
-          </h1>
-        </v-card-title>
-        <p class="text-body-1 text-amber-lighten-3 mt-1">
-          Software Engineer
-          <a
-            class="text-decoration-none text-color-amber-lighten-3"
-            href="https://lifebox.cl"
-            target="blank"
-            :style="{ color: 'inherit' }"
-          >
-            @Lifebox
-          </a>
-        </p>
-        <section class="mt-4">
-          <v-btn
-            v-for="social in profiles"
-            :key="social.network"
-            variant="flat"
-            class="text-none rounded-2 mr-2"
-            :prepend-icon="social.icon"
-            :color="social.color"
-            :href="social.url"
-            target="blank"
-          >
-            {{ social.network }}
-          </v-btn>
-        </section>
-      </v-card-item>
-    </v-overlay>
+    <client-only>
+      <v-overlay
+        :model-value="true"
+        contained
+        no-click-animation
+        persistent
+        class="align-end"
+        :scrim="false"
+      >
+        <v-card-item class="ml-4 mb-4">
+          <v-card-title>
+            <h1 class="text-h4 text-white font-weight-bold">
+              Rodrigo Pizarro Regueiro
+            </h1>
+          </v-card-title>
+          <p class="text-body-1 text-amber-lighten-3 mt-1">
+            Software Engineer
+            <a
+              class="text-decoration-none text-color-amber-lighten-3"
+              href="https://lifebox.cl"
+              target="blank"
+              :style="{ color: 'inherit' }"
+            >
+              @Lifebox
+            </a>
+          </p>
+          <section class="mt-4">
+            <v-btn
+              v-for="social in profiles"
+              :key="social.network"
+              variant="flat"
+              class="text-none rounded-2 mr-2"
+              :prepend-icon="social.icon"
+              :color="social.color"
+              :href="social.url"
+              target="blank"
+            >
+              {{ social.network }}
+            </v-btn>
+          </section>
+        </v-card-item>
+      </v-overlay>
+    </client-only>
     <v-img
       width="50%"
       min-width="300"
