1146c93c6f62179a3417ca74e8706320aed57157
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-07-13 18:22:50 -0400

    ➖ No more vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,7 @@
-import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // @see https://vuetifyjs.com/en/getting-started/installation/#using-nuxt-3
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/styles/main.scss"],
   ssr: true,
-  build: {
-    transpile: ["vuetify"],
-  },
-  modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook("vite:extendConfig", (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }));
-      });
-    },
-    //...
-  ],
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
-    },
-  },
 });
