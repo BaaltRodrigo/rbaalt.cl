27aebdcb72b31c0a8f6fac6396259f6be16dc6bf
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-07-14 01:20:33 -0400

    ➕ nuxt ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // @see https://vuetifyjs.com/en/getting-started/installation/#using-nuxt-3
 export default defineNuxtConfig({
+  compatibilityDate: "2024-07-13",
   devtools: { enabled: true },
+  extends: ["@nuxt/ui-pro"],
+  modules: ["@nuxt/ui"],
   ssr: true,
-  modules: ["@nuxt/ui"]
 });
