1ff3037e705b6edc68883e53f91b2b81652d269b
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-07-14 22:34:14 -0400

    🚧 Profile card with daisy ui

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -1,29 +1,38 @@
 <template>
-  <u-card class="relative min-h-[300px]">
+  <article
+    class="relative min-h-[300px] h-auto border-2 rounded-xl bg-purple-500 bg-opacity-50 border-purple-700"
+  >
     <img
       src="/myself.jpg"
       alt="Rodrigo Pizarro profile image"
-      class="absolute h-full right-0 top-0 rounded-r-lg rounded-tl-[50%]"
+      class="absolute inset-a-0 h-full right-0 top-0 rounded-r-xl rounded-tl-[50%]"
     />
-    <section class="absolute bottom-0 pb-4">
+    <section class="absolute left-4 bottom-0 ma-4">
       <h1 class="text-4xl font-bold text-white">Rodrigo Pizarro Regueiro</h1>
-      <p class="text-yellow-100">Software Engineer @Lifebox</p>
+      <p class="text-amber-300">Software Engineer @Lifebox</p>
       <div class="my-4 flex gap-2">
-        <u-button color="blue">
-          <template #leading>
-            <UIcon name="i-mdi-linkedin" size="20" dynamic />
-          </template>
+        <a
+          role="button"
+          class="btn btn-sm btn-info"
+          href="https://www.linkedin.com/in/baaltrodrigo/"
+          target="_blank"
+        >
+          <u-icon name="i-mdi-linkedin" size="20" dynamic></u-icon>
           Linkedin
-        </u-button>
-        <u-button color="gray" icon="i-mdi-github">
+        </a>
+        <a
+          role="button"
+          class="btn btn-sm btn-secondary"
+          icon="i-mdi-github"
+          href="https://github.com/BaaltRodrigo"
+          target="_blank"
+        >
+          <u-icon name="i-mdi-github" size="20" dynamic></u-icon>
           GitHub
-          <template #leading>
-            <UIcon name="i-mdi-github" size="20" dynamic />
-          </template>
-        </u-button>
+        </a>
       </div>
     </section>
-  </u-card>
+  </article>
 </template>
 
 <script setup>
