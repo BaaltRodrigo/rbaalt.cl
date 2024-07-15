d908e7c0d183397be575d704b4c0075c7eddc6f5
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-07-14 22:34:50 -0400

    🍻 this is a checkpoint i think

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <UPage class="pt-10">
-    <UContainer class="grid grid-cols-12 gap-4">
-      <profile-card class="col-span-12 md:col-span-8 md:row-span-2">
+    <UContainer class="grid grid-cols-6 gap-4">
+      <profile-card class="col-span-6 md:col-span-4 md:row-span-2">
       </profile-card>
-      <Placeholder class="rounded-2xl col-span-12 md:col-span-4 h-40">
-      </Placeholder>
       <Placeholder class="rounded-2xl col-span-6 md:col-span-2 h-40">
       </Placeholder>
-      <Placeholder class="rounded-2xl col-span-6 md:col-span-2 h-40">
+      <Placeholder class="rounded-2xl col-span-3 md:col-span-1 h-40">
+      </Placeholder>
+      <Placeholder class="rounded-2xl col-span-3 md:col-span-1 h-40">
       </Placeholder>
     </UContainer>
     <placeholder class="rounded-2xl my-12 w-[600px] mx-auto h-20"></placeholder>
