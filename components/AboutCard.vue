292acc8f084fe84ed9ecae2dd2e62f0833503f65
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-07-17 00:47:16 -0400

    ♻️ simple about card

---
+++ components/AboutCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="card card-compact bg-purple-500 bg-opacity-50">
+    <div class="card-body">
+      <h2 class="card-title text-2xl">About me</h2>
+      <p class="">
+        Judoka and Software Engineer focused on deliver user friendly solutions.
+        I have been working with web technologies for more than 5 years.
+      </p>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,22 @@
 <template>
-  <UPage class="pt-10">
-    <UContainer class="grid grid-cols-6 gap-4">
-      <profile-card class="col-span-6 md:col-span-4 md:row-span-2">
-      </profile-card>
-      <Placeholder class="rounded-2xl col-span-6 md:col-span-2 h-40">
-      </Placeholder>
-      <Placeholder class="rounded-2xl col-span-3 md:col-span-1 h-40">
-      </Placeholder>
-      <Placeholder class="rounded-2xl col-span-3 md:col-span-1 h-40">
-      </Placeholder>
-    </UContainer>
-    <placeholder class="rounded-2xl my-12 w-[600px] mx-auto h-20"></placeholder>
-    <UContainer class="grid grid-cols-3 gap-4">
-      <Placeholder class="col-span-1 rounded-2xl h-40"> </Placeholder>
-      <Placeholder class="col-span-1 rounded-2xl h-40"> </Placeholder>
-      <Placeholder class="col-span-1 rounded-2xl h-40"> </Placeholder>
-    </UContainer>
-    <u-container>
-      <placeholder class="my-12 rounded-2xl h-40"></placeholder>
-    </u-container>
-  </UPage>
+  <UContainer class="pt-10 grid grid-cols-6 gap-4">
+    <profile-card class="col-span-6 md:col-span-4 md:row-span-2">
+    </profile-card>
+    <!-- <Placeholder class="rounded-2xl col-span-6 md:col-span-2 h-40">
+      </Placeholder> -->
+    <about-card class="col-span-6 md:col-span-2"></about-card>
+    <Placeholder class="rounded-2xl col-span-3 md:col-span-1 h-40">
+    </Placeholder>
+    <Placeholder class="rounded-2xl col-span-3 md:col-span-1 h-40">
+    </Placeholder>
+  </UContainer>
+  <placeholder class="rounded-2xl my-12 w-[600px] mx-auto h-20"></placeholder>
+  <UContainer class="grid grid-cols-3 gap-4">
+    <Placeholder class="col-span-1 rounded-2xl h-40"> </Placeholder>
+    <Placeholder class="col-span-1 rounded-2xl h-40"> </Placeholder>
+    <Placeholder class="col-span-1 rounded-2xl h-40"> </Placeholder>
+  </UContainer>
+  <u-container>
+    <placeholder class="my-12 rounded-2xl h-40"></placeholder>
+  </u-container>
 </template>
