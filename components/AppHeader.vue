06be8af572684d01083f09896ec35e9a6c3ef30a
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-04-26 00:20:07 -0400

    ♻️ use tailwind color for header background

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="bg-[#0e1629] border-b border-gray-700 py-4 px-4 md:px-8 fixed top-0 left-0 w-full z-1"
+    class="bg-gray-900 border-b border-gray-700 py-4 px-4 md:px-8 fixed top-0 left-0 w-full z-1"
   >
     <div class="max-w-5xl mx-auto flex items-center justify-between w-full">
       <h1 class="text-2xl font-bold text-white">rbaalt</h1>
