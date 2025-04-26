ea215fcd975c4499d8db644aa4de18697467d10a
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-04-26 01:23:26 -0400

    💄 add padding on timeline header

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -3,7 +3,7 @@
     class="hidden md:flex gap-8 pt-8 border-l border-gray-700 relative items-start"
   >
     <!-- Left -->
-    <header class="sticky top-25 md:min-w-xs">
+    <header class="sticky top-25 md:min-w-xs pb-8">
       <div class="pl-4 relative">
         <div
           class="absolute -left-1 top-[0.25rem] w-2.25 h-2.25 bg-white rounded-full"
