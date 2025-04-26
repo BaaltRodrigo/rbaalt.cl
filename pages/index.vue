6b87d2c29fbf164063f6a952bdd529d68f78a065
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-04-25 23:53:14 -0400

    💄 add padding to the home sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="max-w-5xl mx-auto grid grid-cols-1 gap-3 mt-20 px-3">
+  <section class="max-w-5xl mx-auto grid grid-cols-1 gap-3 mt-20 px-5">
     <home-hero></home-hero>
   </section>
-  <section class="max-w-5xl mx-auto h-screen mt-10 px-3">
+  <section class="max-w-5xl mx-auto mt-10 px-5">
     <h2 class="text-3xl text-slate-200 my-5">Last working experiences</h2>
     <timeline></timeline>
   </section>
