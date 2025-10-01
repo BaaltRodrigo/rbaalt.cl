3b3547f576a67b841d54f187a6d59ee8c34e9675
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-10-01 10:40:21 -0300

    🍻 brutalism

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,26 +1,28 @@
 <template>
   <header
-    class="bg-gray-900 border-b border-gray-700 py-4 px-5 fixed top-0 left-0 w-full z-1"
+    style="backdrop-filter: blur(12px); -webkit-backdrop-filter: blur(12px)"
+    :class="[
+      'fixed top-0 left-0 z-10',
+      'w-full',
+      'py-4 px-5',
+      'flex items-center justify-between',
+    ]"
   >
-    <div
-      class="max-w-[1050px] mx-auto flex items-center justify-between w-full"
+    <a href="/" class="flex items-center gap-2">
+      <img
+        src="/myself.jpg"
+        alt="Profile picture"
+        class="w-10 h-10 rounded-full"
+      />
+      <h1 class="text-2xl font-medium text-slate-800">rbaalt</h1>
+    </a>
+    <a
+      href="https://www.linkedin.com/in/baaltrodrigo"
+      role="button"
+      target="_blank"
+      class="text-sm font-medium text-slate-800 border border-slate-800 rounded-xl px-4 py-2 hover:bg-slate-800 hover:text-slate-200 transition"
     >
-      <a href="/" class="flex items-center gap-2">
-        <img
-          src="/myself.jpg"
-          alt="Profile picture"
-          class="w-10 h-10 rounded-full"
-        />
-        <h1 class="text-2xl font-bold text-white">rbaalt</h1>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/baaltrodrigo"
-        role="button"
-        target="_blank"
-        class="text-sm font-medium text-blue-400 border border-blue-500 rounded-xl px-4 py-2 hover:bg-blue-500 hover:text-white transition"
-      >
-        Contact
-      </a>
-    </div>
+      Contact
+    </a>
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,79 @@
 <template>
-  <app-header class="z-0"></app-header>
-  <section class="max-w-5xl mx-auto grid grid-cols-1 gap-3 mt-30 px-5">
-    <home-hero></home-hero>
+  <!-- <app-header class="z-0"></app-header> -->
+
+  <section
+    id="home"
+    class="relative w-full h-dvh grid grid-rows-2 bg-slate-200"
+  >
+    <!-- <svg
+      class="w-full h-full px-10 hidden md:block"
+      viewBox="0 0 310 50"
+      fill="none"
+    >
+      <text
+        x="50%"
+        y="50%"
+        text-anchor="middle"
+        dominant-baseline="middle"
+        font-size="45"
+        fill="#1e293b"
+      >
+        Rodrigo Pizarro
+      </text>
+    </svg> -->
+    <!-- <h1 class="text-9xl px-5 text-slate-800 md:hidden">
+      <p>Rodrigo</p>
+      <p>Pizarro</p>
+    </h1> -->
+    <div></div>
+    <section class="content-end justify-self-end">
+      <div class="grid md:grid-cols-3 gap-5 p-5 md:p-10">
+        <div class="md:col-span-2 content-end justify-self-end">
+          <h2 class="text-5xl md:text-6xl">A software artisan</h2>
+          <p class="md:text-2xl text-lg text-slate-600 text-balance">
+            Judoka and software
+            <span class="line-through">engineer</span> artisan from Chile with
+            +7 years of experience on software development. Crafting user
+            friendly solutions.
+          </p>
+        </div>
+        <div class="md:col-span-1 content-end justify-self-end">
+          <p class="text-slate-600 text-right md:text-2xl text-lg">
+            Currently at
+          </p>
+          <h3 class="md:text-5xl text-3xl text-right text-slate-800">
+            Lifebox Chile
+          </h3>
+        </div>
+      </div>
+    </section>
   </section>
-  <section class="max-w-5xl mx-auto mt-10 px-5">
-    <h2 class="text-3xl text-slate-200 my-5">Last working experiences</h2>
-    <timeline
-      v-for="(experience, index) in experiences"
-      :key="'experience-' + index"
-      :company="experience.company"
-      :position="experience.title"
-      :dates="experience.date"
-      :image="experience.image"
-      :description="experience.description"
-      :hide-separator="index === experiences.length - 1"
-    ></timeline>
+
+  <section id="experience" class="w-full h-dvh bg-slate-300 p-5 md:p-10">
+    <div class="grid md:grid-cols-2 h-full">
+      <article class="col-span-1 col-start-2 flex flex-col h-full">
+        <h2 class="text-5xl mb-5">Last working experiences</h2>
+        <div class="flex-1 flex flex-col justify-end gap-5">
+          <div v-for="experience in experiences" :key="experience.company">
+            <p class="text-xl opacity-70">{{ experience.date }}</p>
+            <div class="flex items-center gap-2 text-3xl">
+              <p class="">{{ experience.company }}</p>
+              <p class="opacity-50">{{ experience.title }}</p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
   </section>
+
+  <section id="about-me" class="w-full h-dvh bg-slate-200 p-5 md:p-10">
+    <div class="grid md:grid-cols-2 h-full">
+      <article class="col-span-1 flex flex-col h-full">
+        <h2 class="text-5xl mb-5">Something about me</h2>
+      </article>
+    </div>
+  </section>
+  <section id="contact" class="w-full h-screen bg-slate-600"></section>
 
   <section class="w-full mx-auto border-y border-gray-700">
     <div class="max-w-5xl mx-auto px-5 py-10">
@@ -25,20 +83,6 @@
       <home-stack-grid></home-stack-grid>
     </div>
   </section>
-
-  <!-- <div class="border-b-1 border-gray-700"></div> -->
-  <!-- <section class="w-full grid grid-cols-1 border-t border-b border-gray-700">
-    <a
-      :class="[
-        'text-slate-200 font-medium hover:text-sky-200',
-        'py-4 w-full text-center',
-        'hover:bg-gray-700 transition-colors duration-200',
-        'hover:cursor-pointer',
-      ]"
-    >
-      Check out more experiences
-    </a>
-  </section> -->
 
   <section class="h-screen">
     <div class="flex flex-col items-center justify-end h-full px-5 pb-12">
@@ -82,6 +126,13 @@ const experiences = [
     description:
       "Tech lead of the team that launched the first functional product. Actively involved on the decision making and architecture design of the medical tracking app for diabetes and hearth diseases on Chilean CESFAM.",
     image: "./working-with/cronapp.png",
+  },
+  {
+    title: "Software Engineer",
+    company: "Chilean Army",
+    date: "Sept, 2019 - Mar, 2020",
+    description: "",
+    image: "./working-with/chilean-army.png",
   },
 ];
 </script>
