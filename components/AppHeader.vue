660462228067674dbb3dbe5fdc6cde31d9c18160
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-10-01 10:47:21 -0300

    ⚗️ test external page referrer

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -14,9 +14,8 @@
         <h1 class="text-2xl font-bold text-white">rbaalt</h1>
       </a>
       <a
-        href="https://www.linkedin.com/in/baaltrodrigo"
+        href="https://psp.lifebox.cl"
         role="button"
-        target="_blank"
         class="text-sm font-medium text-blue-400 border border-blue-500 rounded-xl px-4 py-2 hover:bg-blue-500 hover:text-white transition"
       >
         Contact
