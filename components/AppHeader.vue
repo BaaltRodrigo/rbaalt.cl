d2dbc52254da0f5f2386c223dadf240d0df30a49
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-10-01 11:11:53 -0300

    ⚗️ change a tag props

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -14,12 +14,14 @@
         <h1 class="text-2xl font-bold text-white">rbaalt</h1>
       </a>
       <a
-        href="https://psp.lifebox.cl/con"
+        href="http://psp.lifebox.cl/con"
         role="button"
-        rel="noopener"
+        rel="external"
+        target="_blank"
+        referrerpolicy="strict-origin-when-cross-origin"
         class="text-sm font-medium text-blue-400 border border-blue-500 rounded-xl px-4 py-2 hover:bg-blue-500 hover:text-white transition"
       >
-        Contact
+        Test PSP
       </a>
     </div>
   </header>
