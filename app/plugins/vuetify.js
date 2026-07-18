e063982483dbe4114129a01e2eef33bc7a630eff
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2026-07-17 22:45:17 -0400

    build: update dependencies and config to use nuxt 4

--- app/plugins/vuetify.js
+++ app/plugins/vuetify.js
@@ -1,15 +1,9 @@
-// import this after install `@mdi/font` package
 import "@mdi/font/css/materialdesignicons.css";
 import "@fontsource/exo-2";
-
-import "~/styles/main.scss";
-// import "vuetify/styles";
 
 import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
-  const vuetify = createVuetify({
-    // ... your configuration
-  });
+  const vuetify = createVuetify({});
   app.vueApp.use(vuetify);
 });
