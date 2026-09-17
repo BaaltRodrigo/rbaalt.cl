1913dc27f106a7ab6b7ee5f02b9cb3b5b67e1cbd
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2026-09-17 20:55:52 -0300

    refactor: add app header with name and social buttons

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container
+    max-width="900px"
+    min-height="60px"
+    class="border-sm d-flex align-center pa-0"
+  >
+    <section>
+      <v-avatar size="60" class="rounded-0 h-100">
+        <img src="/myself.jpg" alt="Rodrigo Pizarro" class="h-100" />
+      </v-avatar>
+      <span class="pl-1 text-body-large"> Rodrigo Pizarro </span>
+    </section>
+
+    <v-spacer />
+
+    <section class="h-100">
+      <v-btn
+        variant="text"
+        class="rounded-0 h-100 border-s-sm"
+        prepend-icon="mdi-linkedin"
+      >
+        LinkedIn
+      </v-btn>
+      <v-btn
+        variant="text"
+        class="rounded-0 h-100 border-s-sm"
+        prepend-icon="mdi-github"
+      >
+        Github
+      </v-btn>
+    </section>
+  </v-container>
+</template>
