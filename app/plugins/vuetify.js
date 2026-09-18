8fd697845cee20f0e9919f51477f2648c49c2b3c
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2026-09-18 14:34:07 -0300

    feat: set default theme to light in Vuetify configuration

--- app/plugins/vuetify.js
+++ app/plugins/vuetify.js
@@ -6,6 +6,9 @@ import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
+    theme: {
+      defaultTheme: "light",
+    },
     defaults: {
       VContainer: {
         class: "border-s-sm border-e-sm border-b-sm",
